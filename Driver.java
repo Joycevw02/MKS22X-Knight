50ee83cafa6b9bd77dec5dcc907fc3b62b03b0f1
joycevw02 <jwang01@stuy.edu>
2019-02-25 14:35:40 -0500

    Added the testing code from website and tested

--- Driver.java
+++ Driver.java
@@ -1,4 +1,5 @@
 public class Driver{
+  /*
   public static void main(String[] args){
     KnightBoard test = new KnightBoard(5,5);
     System.out.println(test);
@@ -6,5 +7,41 @@ public class Driver{
     //System.out.println(test);
     System.out.println(test.countSolutions(0,0));
     System.out.println(test);
+  }
+  */
+  //testcase must be a valid index of your input/output array
+  public static void runTest(int i){
+
+    KnightBoard b;
+    int[]m =   {4,5,5,5,5};
+    int[]n =   {4,5,4,5,5};
+    int[]startx = {0,0,0,1,2};
+    int[]starty = {0,0,0,1,2};
+    int[]answers = {0,304,32,56,64};
+    if(i >= 0 ){
+      try{
+        int correct = answers[i];
+        b = new KnightBoard(m[i%m.length],n[i%m.length]);
+
+        int ans  = b.countSolutions(startx[i],starty[i]);
+
+        if(correct==ans){
+          System.out.println("PASS board size: "+m[i%m.length]+"x"+n[i%m.length]+" "+ans);
+        }else{
+          System.out.println("FAIL board size: "+m[i%m.length]+"x"+n[i%m.length]+" "+ans+" vs "+correct);
+        }
+      }catch(Exception e){
+        System.out.println("FAIL Exception case: "+i);
+
+      }
+    }
+  }
+
+  public static void main(String args[]){
+    runTest(0);
+    runTest(1);
+    runTest(2);
+    runTest(3);
+    runTest(4);
   }
 }
